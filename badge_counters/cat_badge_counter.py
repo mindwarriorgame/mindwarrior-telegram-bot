5f787fe511f89f00fd9da3ace5783532fab3dd90
stillwaiting <bgvozdev@gmail.com>
2024-11-01 18:28:31 +1100

    Add difficulty to cat badge counter

--- badge_counters/cat_badge_counter.py
+++ badge_counters/cat_badge_counter.py
@@ -5,15 +5,19 @@ INTERVAL_SECS = 16 * 3600;
 
 class CatBadgeCounter:
 
-    def on_game_started(self, active_play_time_secs: int, state: Optional[str]) -> Tuple[Optional[str], Optional[str]]:
-        return None, "pending_superhappy," + str(active_play_time_secs + INTERVAL_SECS)
+    def _calculate_interval_secs(self, difficulty):
+        koef = [0.5, 0.75, 1, 1.25, 1.5]
+        return round(INTERVAL_SECS * koef[difficulty])
 
-    def on_formula_updated(self, active_play_time_secs: int,  state: Optional[str]) -> Tuple[Optional[str], Optional[str]]:
+    def on_game_started(self, active_play_time_secs: int, state: Optional[str], difficulty) -> Tuple[Optional[str], Optional[str]]:
+        return None, "pending_happy," + str(active_play_time_secs + self._calculate_interval_secs(difficulty))
+
+    def on_formula_updated(self, active_play_time_secs: int,  state: Optional[str], difficulty) -> Tuple[Optional[str], Optional[str]]:
         return None, state
 
-    def on_prompt(self, active_play_time_secs: int, state: Optional[str])  -> Tuple[Optional[str], Optional[str]]:
+    def on_prompt(self, active_play_time_secs: int, state: Optional[str], difficulty)  -> Tuple[Optional[str], Optional[str]]:
         if state is None:
-            return None, "pending_happy," + str(active_play_time_secs + INTERVAL_SECS)
+            return None, "pending_happy," + str(active_play_time_secs + self._calculate_interval_secs(difficulty))
 
         if state.startswith("pending_superhappy"):
             return None, state.replace("pending_superhappy", "pending_happy")
@@ -21,44 +25,44 @@ class CatBadgeCounter:
         return None, state
 
 
-    def on_penalty(self, active_play_time_secs: int, state: Optional[str]) -> Tuple[Optional[str], Optional[str]]:
+    def on_penalty(self, active_play_time_secs: int, state: Optional[str], difficulty) -> Tuple[Optional[str], Optional[str]]:
         return "c0", None
 
-    def on_review(self, active_play_time_secs: int, state: Optional[str]) -> Tuple[Optional[str], Optional[str]]:
+    def on_review(self, active_play_time_secs: int, state: Optional[str], difficulty) -> Tuple[Optional[str], Optional[str]]:
         if state is None:
-            return None, "pending_superhappy," + str(active_play_time_secs + INTERVAL_SECS)
+            return None, "pending_happy," + str(active_play_time_secs + self._calculate_interval_secs(difficulty))
 
         split = state.split(",")
         fire_at_secs = int(split[1])
 
         if state.startswith("pending_happy") and fire_at_secs < active_play_time_secs:
-            return "c1", "pending_superhappy," + str(active_play_time_secs + INTERVAL_SECS)
+            return "c1", "pending_superhappy," + str(active_play_time_secs + self._calculate_interval_secs(difficulty))
 
         if state.startswith("pending_superhappy") and fire_at_secs < active_play_time_secs:
-            return "c2", "pending_superhappy," + str(active_play_time_secs + INTERVAL_SECS)
+            return "c2", "pending_superhappy," + str(active_play_time_secs + self._calculate_interval_secs(difficulty))
 
         return None, state
 
-    def progress(self, for_badge, active_play_time_secs, state):
+    def progress(self, for_badge, active_play_time_secs, state, difficulty):
         if for_badge != "c1" and for_badge != "c2":
             return None, -1
 
         if state is None:
             if for_badge == "c1":
                 return [{
-                    "remaining_time_secs": INTERVAL_SECS,
+                    "remaining_time_secs": self._calculate_interval_secs(difficulty),
                     "challenge": "review_regularly_no_penalty",
                     "badge": "c1"
                 }], 0
             if for_badge == "c2":
                 return [
                     {
-                        "remaining_time_secs": INTERVAL_SECS,
+                        "remaining_time_secs": self._calculate_interval_secs(difficulty),
                         "challenge": "review_regularly_no_penalty",
                         "badge" : "c1",
                     },
                     {
-                        "remaining_time_secs": INTERVAL_SECS,
+                        "remaining_time_secs": self._calculate_interval_secs(difficulty),
                         "challenge": "review_regularly_no_prompt",
                         "badge": "c2"
                     }
@@ -66,13 +70,13 @@ class CatBadgeCounter:
         if state.startswith("pending_happy"):
             split = state.split(",")
             secs_before_next_badge = max(int(split[1]) - active_play_time_secs, 0)
-            secs_since_prev_badge = max(INTERVAL_SECS - secs_before_next_badge, 0)
+            secs_since_prev_badge = max(self._calculate_interval_secs(difficulty) - secs_before_next_badge, 0)
             if for_badge == "c1":
                 return [{
                     "remaining_time_secs": secs_before_next_badge,
                     "challenge": "review_regularly_no_penalty",
                     "badge": "c1"
-                }], 100 * secs_since_prev_badge // INTERVAL_SECS
+                }], 100 * secs_since_prev_badge // self._calculate_interval_secs(difficulty)
             if for_badge == "c2":
                 return [
                     {
@@ -81,21 +85,31 @@ class CatBadgeCounter:
                         "badge": "c1"
                     },
                     {
-                        "remaining_time_secs": INTERVAL_SECS,
+                        "remaining_time_secs": self._calculate_interval_secs(difficulty),
                         "challenge": "review_regularly_no_prompt",
                         "badge": "c2"
                     }
-                ], 100 * (secs_since_prev_badge + INTERVAL_SECS) // (2 * INTERVAL_SECS)
+                ], 100 * secs_since_prev_badge // (2 * self._calculate_interval_secs(difficulty))
         if state.startswith("pending_superhappy"):
             split = state.split(",")
 
             secs_before_next_badge = max(int(split[1]) - active_play_time_secs, 0)
-            secs_since_prev_badge = max(INTERVAL_SECS - secs_before_next_badge, 0)
+            secs_since_prev_badge = max(self._calculate_interval_secs(difficulty) - secs_before_next_badge, 0)
 
-            return [
-                {
-                    "remaining_time_secs": secs_before_next_badge,
-                    "challenge": "review_regularly_no_penalty" if for_badge == "c1" else "review_regularly_no_prompt",
-                    "badge": for_badge
-                }
-            ], 100 * secs_since_prev_badge // INTERVAL_SECS
+            if for_badge == "c1":
+                return [
+                    {
+                        "remaining_time_secs": secs_before_next_badge,
+                        "challenge": "review_regularly_no_penalty",
+                        "badge": for_badge
+                    }
+                ], 100 * secs_since_prev_badge // self._calculate_interval_secs(difficulty)
+
+            if for_badge == "c2":
+                return [
+                    {
+                        "remaining_time_secs": secs_before_next_badge,
+                        "challenge": "review_regularly_no_prompt",
+                        "badge": for_badge
+                    }
+                ], 50 + (100 * secs_since_prev_badge // self._calculate_interval_secs(difficulty))
--- badge_counters/cat_badge_counter_test.py
+++ badge_counters/cat_badge_counter_test.py
@@ -8,38 +8,97 @@ class TestGameManager(unittest.IsolatedAsyncioTestCase):
     def test_way_to_c1(self):
         counter = CatBadgeCounter()
 
-        result = counter.progress("c1", 0, None)
+        result = counter.progress("c1", 0, None, 2)
         self.assertEqual(result, ([{'badge': 'c1',
                                     'challenge': 'review_regularly_no_penalty',
                                     'remaining_time_secs': 57600}],
                                   0))
 
-        badge_advice = counter.on_review(10, None)
-        self.assertEqual(badge_advice, (None, 'pending_superhappy,57610'))
+        badge_advice = counter.on_review(10, None, 2)
+        self.assertEqual(badge_advice, (None, 'pending_happy,57610'))
         state = badge_advice[1]
 
-        result = counter.progress("c1", 1000, state)
+        result = counter.progress("c1", 1000, state, 2)
         self.assertEqual(result, ([{'badge': 'c1',
                                     'challenge': 'review_regularly_no_penalty',
                                     'remaining_time_secs': 56610}],
                                   1))
 
-        badge_advice = counter.on_prompt(2000, state)
+        badge_advice = counter.on_prompt(2000, state, 2)
         self.assertEqual(badge_advice, (None, 'pending_happy,57610'))
         state = badge_advice[1]
 
-        result = counter.progress("c1", 5000, state)
+        result = counter.progress("c1", 5000, state, 2)
         self.assertEqual(result, ([{'badge': 'c1',
                                     'challenge': 'review_regularly_no_penalty',
                                     'remaining_time_secs': 52610}],
                                   8))
 
-        result = counter.progress("c1", 500000, state)
+        result = counter.progress("c1", 500000, state, 2)
         self.assertEqual(result, ([{'badge': 'c1',
                                     'challenge': 'review_regularly_no_penalty',
                                     'remaining_time_secs': 0}],
                                   100))
 
-        badge_advice = counter.on_review(500000, state)
+        badge_advice = counter.on_review(500000, state, 2)
         self.assertEqual(badge_advice, ('c1', 'pending_superhappy,557600'))
+        state = badge_advice[1]
+
+
+    def test_way_to_c2(self):
+        counter = CatBadgeCounter()
+
+        result = counter.progress("c2", 0, None, 3)
+        self.assertEqual(result, ([{'badge': 'c1',
+                                    'challenge': 'review_regularly_no_penalty',
+                                    'remaining_time_secs': 72000},
+                                   {'badge': 'c2',
+                                    'challenge': 'review_regularly_no_prompt',
+                                    'remaining_time_secs': 72000}],
+                                  0))
+
+        badge_advice = counter.on_review(10, None, 3)
+        self.assertEqual(badge_advice, (None, 'pending_happy,72010'))
+        state = badge_advice[1]
+
+        result = counter.progress("c2", 3000, state, 3)
+        self.assertEqual(result, ([{'badge': 'c1',
+                                    'challenge': 'review_regularly_no_penalty',
+                                    'remaining_time_secs': 69010},
+                                   {'badge': 'c2',
+                                    'challenge': 'review_regularly_no_prompt',
+                                    'remaining_time_secs': 72000}],
+                                  2))
+
+        badge_advice = counter.on_review(73000, state, 3)
+        self.assertEqual(badge_advice, ('c1', 'pending_superhappy,145000'))
+        state = badge_advice[1]
+
+        result = counter.progress("c2", 73000, state, 3)
+        self.assertEqual(result, ([{'badge': 'c2',
+                                    'challenge': 'review_regularly_no_prompt',
+                                    'remaining_time_secs': 72000}],
+                                  50))
+
+        badge_advice = counter.on_prompt(75000, state, 3)
+        self.assertEqual(badge_advice, (None, 'pending_happy,145000'))
+        state = badge_advice[1]
+
+
+        result = counter.progress("c2", 75000, state, 3)
+        self.assertEqual(result, ([{'badge': 'c1',
+                                    'challenge': 'review_regularly_no_penalty',
+                                    'remaining_time_secs': 70000},
+                                   {'badge': 'c2',
+                                    'challenge': 'review_regularly_no_prompt',
+                                    'remaining_time_secs': 72000}],
+                                  1))
+
+        badge_advice = counter.on_review(150000, state, 3)
+        self.assertEqual(badge_advice, ('c1', 'pending_superhappy,222000'))
+        state = badge_advice[1]
+
+
+        badge_advice = counter.on_review(222001, state, 3)
+        self.assertEqual(badge_advice, ('c2', 'pending_superhappy,294001'))
         state = badge_advice[1]
