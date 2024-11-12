7aeacf857b5c85851d5ba78b31bd9cee4039821e
stillwaiting <bgvozdev@gmail.com>
2024-11-13 10:38:58 +1100

    Improve message for grumpy cat

--- miniapp-frontend/board-scripts/progress-popup.js
+++ miniapp-frontend/board-scripts/progress-popup.js
@@ -34,12 +34,23 @@ function openPopup(badge, progress) {
     const reviewBadges = {
         's0': window.lang.review_without_misses_times,
         's1': window.lang.review_without_misses_times,
-        's2': window.lang.review_without_misses_times,
-        'c0': window.lang.review_times
+        's2': window.lang.review_without_misses_times
     }
 
     if (timeBadges[badge]) {
         content += renderReviewWithoutSomething(timeBadges[badge], progress.progress_pct, progress.remaining_time_secs);
+    } else if (badge === 'c0') {
+        const message = window.lang.review_times
+            .replace("##1", Math.ceil(progress.remaining_reviews / 3))
+            .replace("##2", Math.ceil(progress.remaining_reviews / 2))
+            .replace("##3", progress.remaining_reviews);
+        const ret = [
+            '<p>',
+            message,
+            '</p>'
+        ];
+        ret.push(renderProgressBar(progress.progress_pct || 0));
+        content += ret.join(' ');
     } else {
         content += renderReviewWithoutSomething(reviewBadges[badge], progress.progress_pct, progress.remaining_reviews, false);
     }
